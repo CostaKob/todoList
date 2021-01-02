9451f98223f0668ea791cb8e49aca0bd6993fd14
Constantin Kobrinsky <costa@Constantins-MacBook-Air.local>
2021-01-02 14:46:31 +0200

    todolist

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import TodoList from './Todo/TodoList'
+import Context from './context'
+import AddTodo from './Todo/AddTodo';
+import Loader from './Loader'
 
 function App() {
+
+  const [todos, setTodos] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/todos?_limit=5')
+      .then(response => response.json())
+      .then(todos => {
+        setTodos(todos)
+        setLoading(false)
+      })
+  }, [])
+
+  function toggleTodo(id) {
+    setTodos(todos.map(todo => {
+      if (todo.id === id) {
+        todo.completed = !todo.completed;
+      }
+      return todo;
+    }))
+  }
+
+  function removeTodo(id) {
+    setTodos(todos.filter(todo => todo.id !== id))
+  }
+
+  function addTodo(title) {
+    setTodos([
+      ...todos, 
+      { title, id: Date.now(), completed: false }
+    ])
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Context.Provider value={{ removeTodo }}>
+      <div className="wrapper">
+        <h1>React tutorial</h1>
+        <AddTodo onCreate={addTodo}/>
+        {loading && <Loader/>}
+        { todos.length 
+          ?
+          <TodoList todos={todos} onToggle={toggleTodo}/> 
+          : (loading ? null : <p>No todos =(</p>)
+        }
+      </div>
+    </Context.Provider>
+  )
 }
 
 export default App;
---
+++ src/Loader.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Loader = () => {
+    return <div style={{display: 'flex', justifyContent: 'center', margin: '1.5rem'}}><div className={"lds-dual-ring"}></div></div>
+}
+
+export default Loader
